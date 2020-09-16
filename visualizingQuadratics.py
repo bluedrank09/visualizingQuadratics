396119f73df8caa3a906352ac7f736103f966104
mayababu <mayababu73@gmail.com>
2020-09-16 18:01:39 +1200

    Made some changes, the ode doesn't work just yet

--- visualizingQuadratics.py
+++ visualizingQuadratics.py
@@ -12,39 +12,41 @@ def drawGraph(xList, yList):
 
     plt.show() 
 
-def checkEquation(equation):
+def checkEquation(loopnumber, equationList):
     # valueA = what you multiply x^2 by, like 8 times x^2. 8 is a.
     # valueX is the x, which comes from range.
     # the equation is ax^2+bx+c
     try:
         #currentValue = 0
+        for equation in equationList:
+            for i in range(0,loopNumber):
 
-        if not equation[0].isdigit():
-            raise Exception("Missing a number here to use for a")
-        
-        if equation[1] != 'x':
-            raise Exception('Missing the character "x"')
-        
-        if equation[2] != '^':
-            raise Exception('Missing a carot sign ("^")')
+                if not equation[0].isdigit():
+                    raise Exception("Missing a number here to use for a")
+                
+                if equation[1] != 'x':
+                    raise Exception('Missing the character "x"')
+                
+                if equation[2] != '^':
+                    raise Exception('Missing a carot sign ("^")')
 
-        if equation[3] != '2':
-            raise Exception('Please enter a 2')
+                if equation[3] != '2':
+                    raise Exception('Please enter a 2')
 
-        if not (equation[4] == '+' or equation[4] =='-'):
-            raise Exception('Please enter a "+" or "-"')
+                if not (equation[4] == '+' or equation[4] =='-'):
+                    raise Exception('Please enter a "+" or "-"')
 
-        if  not equation[5].isdigit():
-            raise Exception('Please enter a number for b')
-            
-        if equation[6] != 'x':
-            raise Exception('Missing the character "x"')
+                if  not equation[5].isdigit():
+                    raise Exception('Please enter a number for b')
+                    
+                if equation[6] != 'x':
+                    raise Exception('Missing the character "x"')
 
-        if not (equation[7] == '+' or equation[7] =='-'):
-            raise Exception('Please enter a "+" or "-"')
+                if not (equation[7] == '+' or equation[7] =='-'):
+                    raise Exception('Please enter a "+" or "-"')
 
-        if  not equation[8].isdigit():
-            raise Exception('Please enter a number for c')
+                if  not equation[8].isdigit():
+                    raise Exception('Please enter a number for c')
 
         return(True)
 
@@ -54,31 +56,32 @@ def checkEquation(equation):
         raise error
 
 
-def doCalculations(equation ,numberForRange):
+def doCalculations(equationList ,numberForRange, loopNumber):
     xList = []
     yList = []
 
-    for number in range(-numberForRange, numberForRange+1):
-        valueA = int(equation[0])
-        valueB = int(equation[5])
-        valueC = int(equation[8])
+    for equation in equationList:
+        for i in range(0,loopNumber):
 
-        firstIteration = int(valueA*number**2)
+            for number in range(-numberForRange, numberForRange+1):
+                valueA = int(equation[0])
+                valueB = int(equation[5])
+                valueC = int(equation[8])
 
-        if equation[4] == '+':
-            secondIteration = int(firstIteration + valueB*number)
-        elif equation[4] == '-':
-            secondIteration = int(firstIteration - valueB*number)
-        
-        if equation[7] == '+':
-            thirdIteration = int(secondIteration + valueC*number)
-        elif equation[7] == '-':
-            thirdIteration = int(secondIteration - valueC*number)
+                firstIteration = int(valueA*number**2)
 
-        xList.append(number)
-        yList.append(thirdIteration)
-    print(xList)
-    print(yList)
+                if equation[4] == '+':
+                    secondIteration = int(firstIteration + valueB*number)
+                elif equation[4] == '-':
+                    secondIteration = int(firstIteration - valueB*number)
+                
+                if equation[7] == '+':
+                    thirdIteration = int(secondIteration + valueC*number)
+                elif equation[7] == '-':
+                    thirdIteration = int(secondIteration - valueC*number)
+
+                xList.append(number)
+                yList.append(thirdIteration)
 
     drawGraph(xList, yList)
         
@@ -86,10 +89,17 @@ def doCalculations(equation ,numberForRange):
 
 if __name__ == "__main__":
     try:
-        equation = input("Give me the equation that I will use (must fit format ax^2 +/- bx +/- c) : ")
+
+        equationList =[]
+
+        loopNumber = int(input("How many eqautions do you want me to visualize? : "))
+
+        for i in range(0,loopNumber):
+            equation = input("Give me the equation that I will use (must fit format ax^2 +/- bx +/- c) : ")
+            equationList.append(equation)
         numberForRange = int(input(f"Give me a number to use as the maximum for the range of numbers : "))
-        checkEquation(equation)
-        doCalculations(equation, numberForRange)
+        checkEquation(loopNumber, equationList)
+        doCalculations(equationList, numberForRange, loopNumber)
 
     except Exception as error:
         print(error)
